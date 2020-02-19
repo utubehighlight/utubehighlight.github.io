7b6e23c1f686158e4386d30aa6096744051f968d
Bolaji Makinde <bmakinde@calpoly.edu>
2020-02-19 14:36:28 -0800

    web

---
+++ index.js
@@ -0,0 +1,21 @@
+const cv = require('opencv4nodejs');
+const path = require('path');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const FPS = 10;
+const wCap = new cv.VideoCapture(0);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+setInterval(() => {
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('image', image);
+}, 1000 / FPS)
+
+server.listen(3000);
